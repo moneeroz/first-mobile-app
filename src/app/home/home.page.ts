66cf6c189a10a093d21778d846092e3710bbf4b6
Moneer <moneer.alromimah@gmail.com>
2023-04-13 17:23:20 -0600

    imported the router module to make routerlink work

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { IonicModule, NavController } from '@ionic/angular';
 
 @Component({
@@ -7,7 +7,7 @@ import { IonicModule, NavController } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, RouterModule],
 })
 export class HomePage {
   constructor(private navCtl: NavController, private router: Router) {}
